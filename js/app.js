b67bafeec74b8e7f6fbb6b7e86c5277bdbb81d12
AliAfzal95 <109174608+AliAfzal95@users.noreply.github.com>
2025-12-24 18:46:03 +0500

    Include network type in createOrder function
    Added network type 'TRC20' to order creation data.

--- js/app.js
+++ js/app.js
@@ -13,6 +13,8 @@ const PaymentAPI = {
     },
 
     async createOrder(data) {
+        data.network = 'TRC20';
+
         const response = await fetch(`${API_BASE_URL}/api/payment/create-order`, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
